f2ca682d24cb9ee47e78cb3d55962898f190dbb8
Adrian-Oliveira <adrianalves483@gmail.com>
2024-04-03 17:44:00 -0300

    basic elements editProfilePage

--- src/pages/EditProfilePage/EditProfilePage.tsx
+++ src/pages/EditProfilePage/EditProfilePage.tsx
@@ -1,11 +1,19 @@
+import './editProfilePage.scss'
 import TopNav from '../../components/TopNav';
+import { Link } from 'react-router-dom';
 const EditProfilePage = ()=> {
 
     return(
         <>
             <TopNav/>
             <div className='editProfilePage'>
-                <h1>editProfilePage</h1>
+                <Link to='/profile' className='editProfilePage__back'>
+                    <i className="material-symbols-outlined">
+                        arrow_back_ios
+                    </i>
+                    Back
+                </Link>
+                <div className="editProfilePage__edit"></div>
             </div>
         </>
     );
