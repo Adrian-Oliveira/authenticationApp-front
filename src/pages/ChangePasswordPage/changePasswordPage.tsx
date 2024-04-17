e688b1b0d23f1843dcbb4cc9dfd32fe0ebf4115f
Adrian-Oliveira <adrianalves483@gmail.com>
2024-04-17 17:59:53 -0300

    verify if the new password and the confirmation are equal

--- src/pages/ChangePasswordPage/changePasswordPage.tsx
+++ src/pages/ChangePasswordPage/changePasswordPage.tsx
@@ -3,10 +3,14 @@ import TopNav from '../../components/TopNav';
 import { Link } from 'react-router-dom';
 
 
-import { useAppSelector } from '../../core/hooks';
+import { useAppSelector} from '../../core/hooks';
+import { useState } from 'react';
 
 const ChangePasswordPage = ()=> {
 
+    const [oldPssWrd ,setOldPssWrd]= useState<string>('')
+    const [newPssWrd ,setNewPssWrd]= useState<string>('')
+    const [repeatNewPssWrd ,setRepeatNewPssWrd]= useState<string>('')
 
     return(
         <>
@@ -25,19 +29,40 @@ const ChangePasswordPage = ()=> {
                         <label className='changePassword__edit__input'>
                             <div className='changePassword__edit__inputName'>Old Password</div>
                             <input className='changePassword__edit__inputValue'
-                            type="password" name="" id="" placeholder='Enter your old password...'/>
+                            type="password" 
+                            name="OldPassword"  
+                            placeholder='Enter your old password...'
+                            onChange={(e)=>setOldPssWrd(e.target.value)}
+                            value={oldPssWrd}/>
                         </label> 
 
                         <label className='changePassword__edit__input'>
                             <div className='changePassword__edit__inputName'>New Password</div>
                             <input className='changePassword__edit__inputValue'
-                            type="password" name="" id="" placeholder='Enter your new password...'/>
+                            type="password" 
+                            name="NewPassword"
+                            placeholder='Enter your new password...'
+                            onChange={(e)=>setNewPssWrd(e.target.value)}
+                            value={newPssWrd} />
                         </label>
 
                         <label className='changePassword__edit__input'>
-                            <div className='changePassword__edit__inputName'>Repeat New Password</div>
-                            <input className='changePassword__edit__inputValue'
-                            type="password" name="" id="" placeholder='Enter your new password...' />
+                            <div className='changePassword__edit__inputName'>Confirm New Password</div>
+                            <input 
+                            className={`changePassword__edit__inputValue 
+                            ${(newPssWrd === repeatNewPssWrd)?null:'changePassword__edit__inputValue--notEqual'}`}
+                            type="password" 
+                            name="Repeat New Password" 
+                            placeholder='Enter your confirm new password...' 
+                            onChange={(e)=>setRepeatNewPssWrd(e.target.value)}
+                            value={repeatNewPssWrd}/>
+                            
+                            <p 
+                            className={`changePassword__edit__error 
+                            ${(newPssWrd === repeatNewPssWrd)?
+                                '':
+                                'changePassword__edit__error--active'}`}>
+                            The new password and the confirmation need to be equal</p>
                         </label>
 
                         <button className='changePassword__edit__button'>Change Password</button>
