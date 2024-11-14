6951556108c34908c738a3116a54eafe34e25471
adrian <adrian.oliveira.dev@gmail.com>
2024-11-14 01:57:12 -0300

    postRegister with axios

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,4 +1,5 @@
 const baseUrl = import.meta.env.VITE_BACKEND_API_URL;
+import axios from "axios";
 export default {
   delTwoFactor:()=>{},
   postTwoFactor:()=>{},
@@ -6,12 +7,30 @@ export default {
   postProfile:()=>{},
   getProfile:()=>{},
   recoverPassword:()=>{},
-  loginWithEmail:()=>{},
-  register: async(email:String, password:String)=>{
+  postLoginWithEmail:()=>{},
+  postRegister: async(email:String, password:String)=>{
     try{
 
+      const now = new Date();
+
+      // Get the current timestamp in milliseconds
+      const timestamp = now.getTime();
+
+      // Create a Date object with the specified timezone
+      const dateWithTimezone = new Date(timestamp);
+      dateWithTimezone.setMinutes(dateWithTimezone.getMinutes() - dateWithTimezone.getTimezoneOffset());
+
+      // Format the timestamp as ISO 8601 string with timezone
+      const isoTimestamp = dateWithTimezone.toISOString();
+
+      console.log(isoTimestamp);
+
+      const res = await axios.post(`${baseUrl}/register`,{email, password, timestamp:isoTimestamp})
+      return res
     }
     catch(err){
+
+      console.log(err)
       
     }
   }
