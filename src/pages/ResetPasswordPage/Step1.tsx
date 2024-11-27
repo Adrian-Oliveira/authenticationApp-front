26eb92b676cbfc66fddf646605451dc35d5ae139
adrian <adrian.oliveira.dev@gmail.com>
2024-11-26 23:56:42 -0300

    successfully ask for reset password token

--- src/pages/ResetPasswordPage/Step1.tsx
+++ src/pages/ResetPasswordPage/Step1.tsx
@@ -1,9 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { useToasts } from "react-toast-notifications";
+import api from "../../core/api";
+import { useState } from "react";
 
 export const Step1 = ()=>{
     const navigate = useNavigate();
 
-    const getToken2ResetPassword = 
+    const { addToast } = useToasts();
+
+    const [email, setEmail] = useState<String>('')
+
+    const generateToken2ResetPassword = useMutation({
+        mutationFn: (user:{email: String})=> api.postGenerateTokenToResetPassword(user.email),
+        onError: (error, variables, context) => {
+            // An error happened!
+            const msg = error.response?.data.message ? error.response.data.message: error.message ;
+            addToast(`${msg}`, { appearance: 'error' });
+        },
+        onSuccess(data, variables, context) {
+            addToast(`${data.data.message}`, { appearance: 'success' });
+            navigate('../login')
+        },
+
+    })
 
     return(
         <>
@@ -16,12 +36,13 @@ export const Step1 = ()=>{
                     type="email" name="" id="resetPasswordPage__resetPasswordContainer__email"
                     defaultValue={''}
                     placeholder='Email' 
-                    />
+                    onChange={(e)=>setEmail(e.target.value)}
+                />
             </label>
 
             <button
                 className='resetPasswordPage__resetPasswordContainer__loginButton'
-                onClick={()=>navigate('../step2')}
+                onClick={()=>generateToken2ResetPassword.mutate({email})}
                 >Send the code to my email
             </button>
         </>
--- src/pages/ResetPasswordPage/Step2.tsx
+++ src/pages/ResetPasswordPage/Step2.tsx
@@ -1,31 +1,68 @@
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useToasts } from "react-toast-notifications";
+import api from "../../core/api";
 
 export  const Step2 = ()=>{
     const navigate = useNavigate();
 
+
+    const { addToast } = useToasts();
+
+
+    const [token, setToken] = useState<String>('')
+    const [newPassword, setNewPassword] = useState<String>('')
+    const [confirmNewPassword, setConfirmNewPassword] = useState<String>('')
+
+    const changePassword = useMutation({
+        mutationFn: (user:{token: String, newPassword:String})=> api.postNewPasswordWithToken(user.token, user.newPassword),
+        onError: (error, variables, context) => {
+            // An error happened!
+            const msg = error.response?.data.message ? error.response.data.message: error.message ;
+            addToast(`${msg}`, { appearance: 'error' });
+        },
+        onSuccess(data, variables, context) {
+            addToast(`${data.data.message}`, { appearance: 'success' });
+            navigate('../login')
+        },
+
+    })
+
+    const handleAttemptToChangePassword = ()=>{
+        if(newPassword !== confirmNewPassword){
+            addToast('New Password and Confirm New Password does not match', { appearance: 'error' });
+        }
+        else{
+            changePassword.mutate({token, newPassword})
+        }
+    }
+
     return(
         <>
-            <label 
-                className='resetPasswordPage__resetPasswordContainer__email'>
-                <i className="material-symbols-outlined">
-                    lock
-                </i>
-                <input  
-                    type="password" name="" id="resetPasswordPage__resetPasswordContainer__code"
-                    defaultValue=''
-                    placeholder='Code' 
-                    />
-            </label>
-
-                    <label 
+        <label 
             className='resetPasswordPage__resetPasswordContainer__email'>
             <i className="material-symbols-outlined">
-                mail
+                token
+            </i>
+            <input  
+                type="password" name="" id="resetPasswordPage__resetPasswordContainer__code"
+                onChange={(e)=>setToken(e.target.value)}
+                defaultValue=''
+                placeholder='Code' 
+                />
+        </label>
+
+        <label 
+            className='resetPasswordPage__resetPasswordContainer__email'>
+            <i className="material-symbols-outlined">
+                lock
             </i>
             <input  
                 type="password" name="" id=""
+                onChange={(e)=>setNewPassword(e.target.value)}
                 defaultValue=''
-                placeholder='Password' 
+                placeholder='New Password' 
                 />
         </label>
 
@@ -33,19 +70,20 @@ export  const Step2 = ()=>{
         <label 
             className='resetPasswordPage__resetPasswordContainer__email'>
             <i className="material-symbols-outlined">
-                mail
+                lock
             </i>
             <input  
                 type="password" name="" id=""
+                onChange={(e)=>setConfirmNewPassword(e.target.value)}
                 defaultValue=''
-                placeholder='Confirm the password' 
+                placeholder='Confirm New Password' 
                 />
         </label>
 
             
         <button
             className='resetPasswordPage__resetPasswordContainer__loginButton'
-            onClick={()=>navigate('../login')}
+            onClick={()=>handleAttemptToChangePassword()}
             >Change the password 
         </button>
 
